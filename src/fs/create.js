98eead81593b984b5b9dcbe70234212dc8d7b46a
alex <emal@damain.com>
2023-06-16 15:26:27 +0200

    feat: add fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-    // Write your code here 
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { promises as fsPromises } from "fs";
+import { throws } from "assert";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = join(__dirname, "files", "fresh.txt");
+
+const create = async (content = "I am fresh and young") => {
+  try {
+    const result = await fsPromises.access(file);
+    if (result === undefined) {
+      // means file exist
+      throw new Error("FS operation failed");
+    }
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      // means file not exist
+      await fsPromises.writeFile(file, content);
+    } else throw error;
+  }
 };
 
 await create();
