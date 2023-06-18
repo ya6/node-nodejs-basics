5cb6fd71b5aff753a7fdbbdd8268c94ec6867a97
alex <emal@damain.com>
2023-06-18 12:59:03 +0200

    feat: add zip  block solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compress = async () => {
+  const src_file = join(__dirname, "files", "_fileToCompress.txt");
+  const dest_file = join(__dirname, "files", "archive.gz");
+
+  await pipeline(createReadStream(src_file), createGzip(), createWriteStream(dest_file));
+};
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip, createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const src_file = join(__dirname, "files", "archive.gz");
+  const dest_file = join(__dirname, "files", "fileToCompress.txt");
+  await pipeline(createReadStream(src_file), createGunzip(), createWriteStream(dest_file));
 };
 
 await decompress();
