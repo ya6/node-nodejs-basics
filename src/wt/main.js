2a717407ebb8b19fbf1f385eb2fe840dcb595376
alex <emal@damain.com>
2023-06-18 16:39:55 +0200

    feat: add workers block solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,54 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { promises as fsPromises } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const host_cpus = cpus().length;
+  const workers = {};
+  const results = [];
+  let count = 1;
+  let done_count = 0;
+  try {
+    while (count <= host_cpus) {
+      await fsPromises.cp(join(__dirname, "worker.js"), join(__dirname, `worker_${count}.js`));
+      workers[`${count}`] = new Worker(join(__dirname, `worker_${count}.js`));
+
+      workers[`${count}`].postMessage(9 + count);
+
+      workers[`${count}`].on("message", function (message) {
+        results.push({ data: message, status: "resolved", id: this.threadId });
+        this.terminate();
+      });
+
+      workers[`${count}`].on("error", (error) => {
+        results.push({ data: null, status: "error" });
+        this.terminate();
+      });
+
+      workers[`${count}`].on("exit", (exitCode) => {
+        done_count += 1;
+        if (done_count === host_cpus) {
+          const sorted = results.sort((a, b) => {
+            return a.id < b.id ? -1 : 1;
+          });
+          console.log(
+            sorted.map((el) => {
+              return { data: el.data, status: el.status };
+            })
+          );
+        }
+      });
+
+      count += 1;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { parentPort } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (data) => {
+    // console.log("Message received from main script", data);
+    const workerResult = nthFibonacci(data);
+    // console.log("Posting message back to main script");
+    parentPort.postMessage(workerResult);
+    
+  });
 };
 
 sendResult();
